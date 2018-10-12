602a7ab6bb94d1503d65c74bb55873f8e53d4b33
Vu Cao <cdhvu@yahoo.com>
2018-10-12 10:37:00 -0700

    Completed ex2

--- hash-tables/ex2/ex2.py
+++ hash-tables/ex2/ex2.py
@@ -1,6 +1,21 @@
 def reconstruct_trip(tickets):
-  pass 
+    ht = {}
+    for i in tickets:
+        if i[0] is None:
+            ht['start'] = i[1]
+        else:
+            ht[i[0]] = i[1]
+    source = ht['start']
+    trip = []
+    while source is not None:
+        trip.append(source)
+        try:
+            source = ht[source]
+        except KeyError:
+            return []
+    return trip
+
 
 if __name__ == '__main__':
-  # You can write code here to test your implementation using the Python repl
-  pass
+    # You can write code here to test your implementation using the Python repl
+    pass
