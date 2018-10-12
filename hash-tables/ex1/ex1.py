60e79f066e6d9c15df10c0460006c913a50ce520
Vu Cao <cdhvu@yahoo.com>
2018-10-12 10:14:08 -0700

    Completed ex1

--- hash-tables/ex1/ex1.py
+++ hash-tables/ex1/ex1.py
@@ -1,6 +1,20 @@
 def get_indices_of_item_weights(weights, limit):
-  pass 
+    # ht ={weights[i]:i for i in range(len(weights))}
+    ht = {}
+    for i in range(len(weights)):
+        if weights[i] not in ht:
+            ht[weights[i]] = [i]
+        else:
+            ht[weights[i]].append(i)
+
+    for k, v in ht.items():
+        if (limit-k) == k:
+            return (ht[k][1], ht[k][0])
+        if (limit-k) in ht:
+            return (v[0], ht[limit-k][0]) if v[0] > ht[limit-k][0] else (ht[limit-k][0], v[0])
+    return ()
+
 
 if __name__ == '__main__':
-  # You can write code here to test your implementation using the Python repl
-  pass 
+    # You can write code here to test your implementation using the Python repl
+    pass
